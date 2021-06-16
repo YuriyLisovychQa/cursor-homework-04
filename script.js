6daaaa3e48a808c917b4445902b46796b5cc47a8
Yurii Lisovych <yurii.lisovych.se@gmail.com>
2021-06-16 18:02:08 +0300

    complete first task

---
+++ script.js
@@ -0,0 +1,9 @@
+const STUDENTS = ["Олександр", "Ігор", "Олена", "Іра", "Олексій", "Світлана"];
+const THEMES = ["Диференційне рівняння", "Теорія автоматів", "Алгоритми і структури даних"];
+const MARKS = [4, 5, 5, 3, 4, 5];
+
+const getPairs = (STUDENTS) => [[STUDENTS[0], STUDENTS[3]], [STUDENTS[1], STUDENTS[2]], [STUDENTS[4], STUDENTS[5]]];
+console.log(getPairs(STUDENTS));
+
+
+
