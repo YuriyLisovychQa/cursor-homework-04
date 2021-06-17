e1aa2bf197bf22e6c4c9778a31eed0f22305550b
Yurii Lisovych <yurii.lisovych.se@gmail.com>
2021-06-17 18:19:44 +0300

    implement fourth task

--- script.js
+++ script.js
@@ -6,24 +6,49 @@ const getPairs = (students) => {
     return [[students[0], students[3]], [students[1], students[2]], [students[4], students[5]]];
 }
 
-
-const joinStudentNames = (students) => {
+const aasignTaskToStudents = (students) => {
     let studentsTasks = [];
+
     for(let i = 0; i < students.length; i++) {
         studentsTasks.push([students[i].join(" і ")]);
         studentsTasks[i].push(THEMES[i]);
     }
+
     return studentsTasks;
 }
-joinStudentNames(getPairs(STUDENTS));
-console.log(joinStudentNames(getPairs(STUDENTS)))
 
 const evaluetionStudents = (students) => {
     let studentsMarks = [];
+
     for(let i = 0; i < students.length; i++) {
         studentsMarks.push([students[i], MARKS[i]]);
     }
+
     return studentsMarks;
 }
-evaluetionStudents(STUDENTS);
-console.log(evaluetionStudents(STUDENTS));
+
+const getRandomMark = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+const evaluetionStudentsProjects = (students) => {
+    let studentsProjectMark = [];
+
+    for(let i = 0; i < students.length; i++) {
+        studentsProjectMark.push(students[i]);
+        studentsProjectMark[i].push(getRandomMark(1, 5));
+    }
+
+    return studentsProjectMark;
+}
+
+const groupStudentsData = () => {
+    let joinedStudentsByGender = getPairs(STUDENTS)
+    let studentsWithAssignedTask = aasignTaskToStudents(joinedStudentsByGender)
+    let studentEvaluetion = evaluetionStudents(STUDENTS)
+    let evaluetionPerProject = evaluetionStudentsProjects(studentsWithAssignedTask)
+
+    console.log(joinedStudentsByGender, studentsWithAssignedTask, studentEvaluetion, evaluetionPerProject);
+}
+
+groupStudentsData();
